d3443012901157eefc3beb056b2bf2324de4e1a2
Cat5671 <152156513+Cat5671@users.noreply.github.com>
2025-11-10 07:50:58 +0300

    feat: add 3

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,13 @@ const { NotImplementedError } = require('../lib');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false
+  return members.reduce((accumulator, member) => {
+    if ((typeof member) == 'string')
+      accumulator.push(member.trim().toUpperCase()[0])
+    return accumulator
+  }, []).sort((a, b) => a.localeCompare(b)).join('')
 }
 
 module.exports = {
