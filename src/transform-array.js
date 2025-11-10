222ef29f9c2f3d53bbde041e1898f8a361f6b91a
Cat5671 <152156513+Cat5671@users.noreply.github.com>
2025-11-10 07:57:37 +0300

    feat: add 8

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,30 @@ const { NotImplementedError } = require('../lib');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  *
  */
-function transform(/* arr */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+  if (!Array.isArray(arr)) throw Error
+    let accumulator = []
+    for (let index = 0; index < arr.length; index++) {
+        const currentValue = arr[index];
+        switch (currentValue) {
+            case discardNext:
+                if (index < arr.length - 1) index++;
+                break;
+            case discardPrev:
+                if (index > 0) accumulator.pop()
+                break;
+            case doubleNext:
+                if (index < arr.length - 1) accumulator.push(arr[index + 1])
+                break;
+            case doublePrev:
+                if (index != 0) accumulator.push(arr[index - 1])
+                break
+            default:
+                accumulator.push(currentValue)
+                break;
+        }
+    }
+    return accumulator;
 }
 
 module.exports = {
