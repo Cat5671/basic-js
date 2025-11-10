c5076b0dbb1677768ceac6964f625528f0639bda
Cat5671 <152156513+Cat5671@users.noreply.github.com>
2025-11-10 07:47:05 +0300

    feat: add first task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,19 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity != 'string') return false
+
+  const numberActivity = parseFloat(sampleActivity)
+  if (!(numberActivity && numberActivity > 0 && numberActivity < MODERN_ACTIVITY)) return false;
+
+  let N_N0 = MODERN_ACTIVITY / numberActivity;
+
+  let k = 0.693 / HALF_LIFE_PERIOD
+
+  let t = Math.log(N_N0) / k
+
+  return Math.ceil(t)
 }
 
 module.exports = {
